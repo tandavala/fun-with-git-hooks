f2d7402a4742d95b13aa9b00d8df90b30d3dda91
tandavala <jose.tandavala@gmail.com>
2020-03-26 13:33:21 +0100

    adding ui

---
+++ build/precache-manifest.a90c5d9eb86012bd3df5d7bca15258e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03ec48b4f4d8dbe353d6b97b96c360b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3520cd991b44bb788e7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2063ef78561668c551de",
+    "url": "/static/js/2.b035ff3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b035ff3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3520cd991b44bb788e7",
+    "url": "/static/js/main.e566ff6f.chunk.js"
+  },
+  {
+    "revision": "a8d2aab337586eba5dc9",
+    "url": "/static/js/runtime-main.e03428f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
